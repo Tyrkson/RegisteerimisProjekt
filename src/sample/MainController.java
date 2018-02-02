f605a16f4d15084fe733582a8c9c4863c0a41b33
Ahto Türkson <ahto.tyrkson@gmail.com>
2018-02-03 01:10:05 +0200

    Version 0.2 koodi pool

---
+++ src/sample/MainController.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    public void loadGameSheet() throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+}
