f30d7ca10f64727b11ec3a46c77fdb940cdc8e7d
Ahto Türkson <ahto.tyrkson@gmail.com>
2018-02-03 13:58:20 +0200

    Version 0.6 UnlockScreenController

--- src/sample/UnlockScreenController.java
+++ src/sample/UnlockScreenController.java
@@ -2,8 +2,12 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -22,8 +26,17 @@ public class UnlockScreenController {
     @FXML
     private AnchorPane patternPane;
 
-    public ArrayList<int[]> patterns;
-    public int[] selectedPattern;
+    @FXML
+    private Circle c1, c2, c3, c4, c5, c6, c7, c8, c9;
+
+    private ArrayList<Circle> circles;
+
+    private ArrayList<Circle> userPattern;
+
+    private ArrayList<Line> lines;
+
+    public ArrayList<String[]> patterns;
+    public String[] selectedPattern;
 
 
     private String file = "patterns.txt";
@@ -32,6 +45,85 @@ public class UnlockScreenController {
         loadPatternDataFromFileToList();
         selectPattern();
         showRequestedPattern();
+        createCircleList();
+
+        patternPane.setOnMouseDragged(e->{
+            if(userPattern == null) userPattern = new ArrayList<>();
+            checkCollision(e.getX(), e.getY());
+
+        });
+
+        patternPane.setOnMouseReleased(e->{
+            boolean result = checkUserPattern(userPattern);
+            if(result){
+                try {
+                    loadGameSheet();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }else {
+                removeLines();
+            }
+        });
+    }
+
+    private void removeLines() {
+
+        for(Line l: lines){
+            patternPane.getChildren().remove(l);
+        }
+    }
+
+    private void createCircleList() {
+        circles = new ArrayList<>();
+        circles.add(c1);
+        circles.add(c2);
+        circles.add(c3);
+        circles.add(c4);
+        circles.add(c5);
+        circles.add(c6);
+        circles.add(c7);
+        circles.add(c8);
+        circles.add(c9);
+
+    }
+
+    private void checkCollision(double x, double y) {
+        for(Circle c : circles){
+            if((c.getCenterX() + c.getRadius()) > x && (c.getCenterX() - c.getRadius()) < x){
+                if((c.getCenterY() + c.getRadius()) > y && (c.getCenterY() - c.getRadius()) < y){
+                    userPattern.add(c);
+                    drawLine();
+                }
+            }
+        }
+
+    }
+
+    private void drawLine() {
+        if(userPattern.size() > 1){
+            int size = userPattern.size();
+            int index1 = size - 2;
+            int index2 = size - 1;
+
+            draw(userPattern.get(index1), userPattern.get(index2));
+        }
+    }
+
+    private void draw(Circle start, Circle end) {
+        if(lines == null) lines = new ArrayList<>();
+
+        Line line = new Line();
+
+        line.setStartX(start.getCenterX());
+        line.setStartY(start.getCenterY());
+        line.setEndX(end.getCenterX());
+        line.setEndY(end.getCenterY());
+
+        line.setStroke(Color.WHITE);
+
+        patternPane.getChildren().add(line);
+        lines.add(line);
     }
 
     private void loadPatternDataFromFileToList() {
@@ -42,11 +134,8 @@ public class UnlockScreenController {
 
             while(line != null){
                 String[] splittedLine = line.split(" ");
-                int[] pattern = new int[splittedLine.length];
-                for(int i = 0; i<splittedLine.length; i++){
-                    pattern[i] = Integer.valueOf(splittedLine[i]);
-                }
-                patterns.add(pattern);
+
+                patterns.add(splittedLine);
                 line = br.readLine();
             }
 
@@ -65,14 +154,25 @@ public class UnlockScreenController {
 
         String pattern = "";
 
-        for(int i: selectedPattern){
+        for(String i: selectedPattern){
             pattern += i + " ";
         }
         lRequestedPattern.setText(pattern);
     }
 
-    private boolean checkUserPattern(int[] userPattern){
-        return userPattern.equals(selectedPattern);
+    private boolean checkUserPattern(ArrayList<Circle> userPattern){
+
+        if(userPattern.size() != selectedPattern.length){
+            return false;
+        }
+
+        for(int i = 0; i<userPattern.size(); i++){
+            if(!userPattern.get(i).getId().equals(selectedPattern[i])){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private void loadGameSheet() throws IOException {
