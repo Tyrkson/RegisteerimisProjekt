9b9058d64a971e57f83eb92a3905748c391b61a0
polaartiim <polaarsaased@gmail.com>
2018-02-04 03:36:04 +0200

    MediaPlayer

---
+++ src/sample/GameMeediaPlayer.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.nio.file.Paths;
+
+public class GameMeediaPlayer{
+    private Media mediasong;
+    private MediaPlayer mediaplayer;
+
+    GameMeediaPlayer(String songName){
+        mediasong = new Media(Paths.get(songName).toUri().toString());
+        mediaplayer = new MediaPlayer(mediasong);
+
+    }
+
+    public void play() {
+        mediaplayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaplayer.play();
+    }
+
+    public void stop() {
+
+        mediaplayer.stop();
+    }
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,9 +8,13 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public  GameMeediaPlayer a;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mein_menu.fxml"));
+        a = new GameMeediaPlayer("src/sample/Introsong/PAtchimoo.mp3");
+        a.play();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
